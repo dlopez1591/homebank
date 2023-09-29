1e21051aa8e7769a59089406b7eb66bcfdee537d
dlopez1591 <dlopez1591@gmail.com>
2023-09-29 18:21:21 -0500

    creado repositorrio y configurado

--- src/main/java/com/daniellopez/homebank/repositories/ClientRepository.java
+++ src/main/java/com/daniellopez/homebank/repositories/ClientRepository.java
@@ -1,4 +1,9 @@
 package com.daniellopez.homebank.repositories;
 
-public interface ClientRepository {
+import com.daniellopez.homebank.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ClientRepository extends JpaRepository <Client, Long> {
 }

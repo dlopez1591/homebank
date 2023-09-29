488cb0b2758fef8fc57f47623c87b9c7c6427578
dlopez1591 <dlopez1591@gmail.com>
2023-09-29 18:06:18 -0500

    creada clase client

---
+++ src/main/java/com/daniellopez/homebank/Client.java
@@ -0,0 +1,16 @@
+package com.daniellopez.homebank;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+}

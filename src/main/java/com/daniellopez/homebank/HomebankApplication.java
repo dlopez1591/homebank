e78ec0e5c8e57d391c50abd101356a0bd48d5f9c
dlopez1591 <dlopez1591@gmail.com>
2023-09-30 14:23:05 -0500

    creado constructor y getters de la clase Client

--- src/main/java/com/daniellopez/homebank/HomebankApplication.java
+++ src/main/java/com/daniellopez/homebank/HomebankApplication.java
@@ -1,13 +1,23 @@
 package com.daniellopez.homebank;
 
+import com.daniellopez.homebank.models.Client;
+import com.daniellopez.homebank.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientRepository){
+		return (args) -> {
+
+		}
 	}
 
 }
--- src/main/java/com/daniellopez/homebank/models/Client.java
+++ src/main/java/com/daniellopez/homebank/models/Client.java
@@ -15,4 +15,23 @@ public class Client {
     private long id;
     private String firstName;
     private String lastName;
+    private String email;
+
+    public Client () {}
+    public Client (String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getEmail(){
+        return email;
+    }
+    
 }

f3908c15bf3a4fd31f2e6c1e2e3b742f0ccf10aa
dlopez1591 <dlopez1591@gmail.com>
2023-09-30 15:11:58 -0500

    creado y configurado repositorio

---
+++ src/main/java/com/daniellopez/homebank/models/Account.java
@@ -0,0 +1,56 @@
+package com.daniellopez.homebank.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String number;
+    private LocalDateTime creationDate;
+    private double balance;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="client_id")
+    private Client client;
+
+    public Account () {}
+    public Account (String number, LocalDateTime creationDate, double balance){
+        this.number = number;
+        this.creationDate = creationDate;
+        this.balance = balance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}

474f3d3b39d0c06d784edd5971238cf3f6a3a164
dlopez1591 <dlopez1591@gmail.com>
2023-10-01 12:22:47 -0500

    finalizado ClientDTO

--- src/main/java/com/daniellopez/homebank/dtos/ClientDTO.java
+++ src/main/java/com/daniellopez/homebank/dtos/ClientDTO.java
@@ -1,17 +1,47 @@
 package com.daniellopez.homebank.dtos;
 
+import com.daniellopez.homebank.models.Account;
 import com.daniellopez.homebank.models.Client;
+
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
 
 public class ClientDTO {
     private long id;
     private String firstName;
     private String lastName;
     private String email;
+    private Set<Account> accounts;
 
     public ClientDTO (Client client){
         this.id = client.getId();
         this.firstName = client.getFirstName();
         this.lastName = client.getLastName();
         this.email = client.getEmail();
+        this.accounts = client.getAccounts().stream()
+                .map(account -> new Account(account.getNumber(), account.getCreationDate(), account.getBalance()))
+                .collect(toSet());
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
     }
 }
--- src/main/java/com/daniellopez/homebank/models/Client.java
+++ src/main/java/com/daniellopez/homebank/models/Client.java
@@ -16,7 +16,7 @@ public class Client {
     private String lastName;
     private String email;
     @OneToMany(mappedBy="client", fetch=FetchType.EAGER)
-    Set<Account> accounts = new HashSet<>();
+    private Set<Account> accounts = new HashSet<>();
 
     public Client () {}
     public Client (String firstName, String lastName, String email){
